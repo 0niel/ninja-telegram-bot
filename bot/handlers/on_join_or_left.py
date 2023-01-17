4e6ee32f00454c72d4aeaed62df799ec7262ccab
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-01-17 13:39:57 +0300

    Update on_join_or_left.py

--- bot/handlers/on_join_or_left.py
+++ bot/handlers/on_join_or_left.py
@@ -2,17 +2,17 @@ import logging
 
 from telegram import Update
 from telegram.ext import ContextTypes, MessageHandler, filters
+from telegram.helpers import escape_markdown
 
-from bot import application, config
+from bot import application
+from bot.filters import IsChatAllowedFilter
 from bot.services import user
+from bot.services.auto_delete import auto_delete
 
 logger = logging.getLogger(__name__)
 
 
 async def on_join(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id != config.get_settings().MIREA_NINJA_GROUP_ID:
-        return
-
     await user.update_user_names(
         update.effective_user.id,
         update.effective_user.username,
@@ -23,25 +23,28 @@ async def on_join(update: Update, context: ContextTypes.DEFAULT_TYPE):
     guidelines_url = "https://mirea.ninja/guidelines"
     title = update.effective_chat.title
 
-    await update.message.reply_markdown_v2(
+    msg = await update.message.reply_html(
         f"Спасибо, что присоединились к {title}, и Добро пожаловать!\n\n"
         f"Этот чат управляется нашим дружелюбным персоналом и вами - сообществом.\n\n"
         f"Если есть критическая или срочная проблема, пожалуйста, свяжитесь с нами в личных сообщениях.\n\n"
-        f"- Мы всегда следуем [основным принципам сообщества]({guidelines_url})."
+        f"- Мы всегда следуем <a href='{guidelines_url}'>основным принципам сообщества</a>.\n"
     )
+
+    auto_delete(msg, context)
 
 
 async def on_left(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id != config.get_settings().MIREA_NINJA_GROUP_ID:
-        return
-
-    await update.message.reply_markdown_v2(
-        f"{update.effective_user.first_name} покинул нас. Надеемся, что мы увидимся снова!"
+    await context.bot.send_message(
+        escape_markdown(
+            f"@{update.effective_user.username} ({update.effective_user.first_name}) покинул нас. Надеемся, что мы "
+            f"увидимся снова!",
+            version=2,
+        )
     )
 
 
 # On join handler
-application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, on_join), group=2)
+application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS & IsChatAllowedFilter(), on_join), group=3)
 
 # On left handler
-application.add_handler(MessageHandler(filters.StatusUpdate.LEFT_CHAT_MEMBER, on_left), group=3)
+application.add_handler(MessageHandler(filters.StatusUpdate.LEFT_CHAT_MEMBER & IsChatAllowedFilter(), on_left), group=3)
